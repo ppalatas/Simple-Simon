df9e0b55ef9ca450babce30cf3da7ab2bf1b04eb
Phillip Palatas <PhillipPalatas@Phillips-MacBook-Pro-3.local>
2017-03-07 23:23:02 -0600

    Cross Referenced grading instructions on slack

---
+++ public/js/SimpleSimon.js
@@ -0,0 +1,139 @@
+$(document).ready(function(){
+    "use strict";
+
+    var sequenceArray = [];
+
+    var usersTurn = [];
+   
+    var counter = 0;
+
+    var round = 0;
+
+    //Start by clicking the button and set the array to empty 
+    $("#start").click(function(){
+   
+        $("#gameOver").removeClass('fadeIn');
+   
+        sequenceArray = [];
+   
+        simonsArray();
+   
+        $("#start").fadeOut(1000);
+    });
+   
+    // Get a random Number 
+    function getRandomNumberBetween0And(){
+          
+          return Math.floor(Math.random() * 4);
+    }
+
+    // Animate the arrow buttons from turning the opacity up and down on the click event.
+    function animate(element){
+          
+          $(element).animate({
+              opacity: .20
+    
+          }, 500).animate({
+                  opacity: 1
+                 
+          });
+    }
+
+    // Game Over is displayed when the user gets the sequence wrong. 
+    function gameover(){
+
+        // Plays Sonic Game Over music.
+        var audio = new Audio('../audio/GameOver.mp3');
+        audio.play();
+        
+        $("#gameOver").addClass('fadeIn');
+
+        $("#start").fadeIn(5000);
+
+        round = 0;
+
+        $("#round").html("Round: " + round);
+
+        //Reset the sequenceArray, usersTurn array and round counter to empty.
+        sequenceArray = [];
+        usersTurn = [];
+    }
+   
+    
+    //logs the Simon's Sequence and compares the Users Input 
+    function compareMove(usersTurn){
+        
+        if (usersTurn != sequenceArray[counter]){
+           
+           gameover();
+
+       } else {
+
+            if(counter == sequenceArray.length -1){
+                
+                round++;
+                $("#round").html("Round: " + round);
+                
+                var audioOot = new Audio('../audio/NextRound.wav');
+                audioOot.play();
+                
+                counter = 0;
+                simonsArray();
+
+            }else{
+
+               counter++;
+
+            }
+        }
+    }
+
+    // Declaring variables to be used for Simon's Array 
+    function simonsArray(input){
+        
+        var squares = ["topButton", "rightButton", "downButton", "leftButton"]; 
+
+        var randomIndex = getRandomNumberBetween0And();
+    
+        var randomSquare = squares[randomIndex];
+
+        sequenceArray.push(randomSquare);
+
+        var i = 0;
+
+        var intervalId = setInterval(function(){
+            
+            switch (sequenceArray[i]){
+                case "topButton": 
+                    animate('#topButton');
+                    break;
+                case "rightButton":
+                    animate('#rightButton');
+                    break;
+                case "downButton":
+                    animate('#downButton');
+                    break;
+                case "leftButton":
+                    animate('#leftButton');
+                    break;
+            }
+
+            if(i < sequenceArray.length){
+                
+                i++;
+   
+            } else {
+   
+                clearInterval(intervalId);
+            }
+   
+        }, 1000);
+    }
+
+    $(".button").click(function(){
+       
+        animate("#" + $(this).attr("id"));
+        compareMove($(this).attr("id"));
+    });
+    
+}); 
